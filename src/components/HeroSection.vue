5dbd2b3d70961e4d384b3cc10b5b6bcbb5447565
Alfrendo Silalahi <alfrendos72@gmail.com>
2026-03-17 14:30:35 +0700

    create simple hero section

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1 class="font-mono text-3xl mb-2">hi there</h1>
+  <p class="font-mono text-lg">
+    i'am a developer who loves to create amazing sofware & tools
+  </p>
+</template>
