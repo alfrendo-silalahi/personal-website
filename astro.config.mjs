a3be8a344de3c37a44a542dc5fe744631ffd5eb5
Alfrendo Silalahi <alfrendos72@gmail.com>
2026-03-18 15:14:30 +0700

    add Lato font family

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import vue from "@astrojs/vue";
-import tailwindcss from "@tailwindcss/vite";
+import vue from '@astrojs/vue';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,6 +1,9 @@
 <template>
   <h1 class="font-mono text-3xl mb-2">hi there</h1>
-  <p class="font-mono text-lg">
+  <p class="font-mono text-lg mb-5">
     i'am a developer who loves to create amazing sofware & tools
   </p>
+  <a class="font-mono underline underline-offset-6 decoration-3" href="/me"
+    >More about me >></a
+  >
 </template>
